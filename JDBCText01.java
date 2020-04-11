6066f0871d3f063269aa77a044d3f53f8356a89f
yangyufen <1170032164@qq.com>
2020-04-11 13:44:04 +0800

    change to the utf-8

--- JDBCText01.java
+++ JDBCText01.java
@@ -15,9 +15,9 @@ public class JDBCText01{
 			String root = "root";
 			String pwd = "1424114642BTS";
 			conn = DriverManager.getConnection(url,root,pwd);
-			System.out.println("���ݿ����"+ conn);
+			System.out.println("数据库连接对象"+ conn);
 			stmt = conn.createStatement();
-			String sql = "insert into dept(deptno,deptname,loc) values(52,'���˲�','����')";
+			String sql = "insert into dept(deptno,deptname,loc) values(52,'人人部','北京')";
 			int count = stmt.executeUpdate(sql);
 			System.out.println(count==1? "success":"fail");
 		}catch(SQLException e){
